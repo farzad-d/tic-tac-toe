68ac5312818539b1ff523dd2e8b203e4a407c976
farzad-d <fzddnp@hotmail.com>
2025-09-15 15:14:50 +0330

    Add turn handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let gameState = {
   board: [],
-  activePlayer: 1,
+  turn: 1,
   status: true,
 };
 
@@ -25,7 +25,6 @@ function createPlayer(name, token) {
       const row = cell[0] - 1;
       const col = cell[1] - 1;
       gameState.board[row][col] ||= token;
-      // updateBoard();
     },
   };
 }
@@ -33,21 +32,12 @@ function createPlayer(name, token) {
 const player1 = createPlayer("Jack", "X");
 const player2 = createPlayer("Bobby", "O");
 
-// player1.choice("22");
-// player2.choice("33");
-// player1.choice("33");
-
-function getChoice() {
-  if (gameState.activePlayer === 1) {
-    const selectedCell = prompt(
-      `It's your turn ${player1.name}, what's your choice?`
-    );
-    player1.choice(selectedCell);
-    // gameState.activePlayer = 2;
-  }
+function activePlayer() {
+  if (gameState.turn === 1) return player1;
+  if (gameState.turn === 2) return player2;
 }
 
-function hasWinner(token) {
+function hasWinner() {
   const c11 = gameState.board[0][0];
   const c12 = gameState.board[0][1];
   const c13 = gameState.board[0][2];
@@ -59,9 +49,9 @@ function hasWinner(token) {
   const c33 = gameState.board[2][2];
 
   function checkLines(line) {
-    if (line.every((c) => c === token)) {
+    if (line.every((c) => c === activePlayer().token)) {
       gameState.status = false;
-      alert("win!");
+      alert(`You won ${activePlayer().name}!`);
     }
   }
 
@@ -73,10 +63,17 @@ function hasWinner(token) {
   checkLines([c13, c23, c33]);
   checkLines([c11, c22, c33]);
   checkLines([c13, c22, c31]);
+
+  gameState.turn = gameState.turn === 1 ? 2 : 1;
+}
+
+function getChoice() {
+  const selectedCell = prompt(`${activePlayer().name}, your move:`);
+  activePlayer().choice(selectedCell);
 }
 
 while (gameState.status) {
   getChoice();
-  hasWinner("X");
   updateBoard();
+  hasWinner();
 }
