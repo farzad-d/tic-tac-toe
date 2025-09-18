d754b04b853a70fea3b510910e716e0873a9e591
farzad-d <fzddnp@hotmail.com>
2025-09-18 19:59:08 +0330

    Add functions to get and manage players info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let gameState = {
   board: [],
   turn: 1,
-  status: true,
+  result: "",
 };
 
 const boardCreator = (function () {
@@ -13,6 +13,25 @@ const boardCreator = (function () {
   }
 })();
 
+const boardDisplay = document.getElementById("game-board");
+
+gameState.board.forEach((row, rowIndex) => {
+  row.forEach((cell, cellIndex) => {
+    const cellBtn = document.createElement("button");
+    cellBtn.classList.add("cell");
+    cellBtn.dataset.coordination = `${rowIndex}${cellIndex}`;
+    boardDisplay.appendChild(cellBtn);
+  });
+});
+
+const cellBtns = document.querySelectorAll(".cell");
+
+function updateBoard() {
+  const flatBoard = gameState.board.flat();
+  cellBtns.forEach((btn, i) => (btn.textContent = flatBoard[i]));
+  gameMessage();
+}
+
 function createPlayer(name, token) {
   return {
     name,
@@ -23,19 +42,40 @@ function createPlayer(name, token) {
   };
 }
 
-const player1 = createPlayer("Jack", "⨯");
-const player2 = createPlayer("Bobby", "〇");
+const form = document.querySelector("form");
 
-function activePlayer() {
-  if (gameState.turn === 1) return player1;
-  if (gameState.turn === 2) return player2;
-}
+const playerControl = {
+  players: [],
+  setPlayers() {
+    const formData = new FormData(form);
+    const p1Name = formData.get("p1-name");
+    const p2Name = formData.get("p2-name");
+    const p1Token = formData.get("p1-token");
+    const p2Token = formData.get("p2-token");
+
+    this.players = [
+      createPlayer(p1Name, p1Token),
+      createPlayer(p2Name, p2Token),
+    ];
+    updateBoard();
+  },
+  activePlayer() {
+    return this.players[gameState.turn - 1];
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  playerControl.setPlayers();
+  form.reset();
+  createPlayerDialog.close();
+});
 
 function getChoice(selectedCell) {
-  activePlayer().choice(selectedCell);
+  playerControl.activePlayer().choice(selectedCell);
 }
 
-function hasWinner() {
+function resultCheck() {
   const lines = [
     [
       [0, 0],
@@ -79,57 +119,46 @@ function hasWinner() {
     ],
   ];
 
-  const checkLines = () =>
+  const winCheck = () =>
     lines.some((line) =>
-      line.every(([x, y]) => gameState.board[x][y] === activePlayer().token)
+      line.every(
+        ([x, y]) => gameState.board[x][y] === playerControl.activePlayer().token
+      )
     );
 
-  if (checkLines()) {
-    gameState.status = false;
+  const drawCheck = () => gameState.board.flat().every((cell) => cell);
+
+  if (winCheck()) {
+    gameState.result = "win";
+  } else if (drawCheck()) {
+    gameState.result = "draw";
   } else {
     gameState.turn = gameState.turn === 1 ? 2 : 1;
   }
 }
 
-// ### DOM ###
-
-const gameBoard = document.getElementById("game-board");
-
-gameState.board.forEach((row, rowIndex) => {
-  row.forEach((cell, cellIndex) => {
-    const cellBtn = document.createElement("button");
-    cellBtn.classList.add("cell");
-    cellBtn.dataset.coordination = `${rowIndex}${cellIndex}`;
-    gameBoard.appendChild(cellBtn);
-  });
-});
-
-const cellBtns = document.querySelectorAll(".cell");
-
-function updateBoard() {
-  const flatBoard = gameState.board.flat();
-  cellBtns.forEach((btn, i) => (btn.textContent = flatBoard[i]));
-  gameMessage();
-}
-
 function gameMessage() {
   const message = document.querySelector("h2");
-  if (gameState.status) {
-    message.textContent = `${activePlayer().name}, your move`;
+
+  if (!gameState.result) {
+    message.textContent = `${playerControl.activePlayer().name}, your move`;
+  } else if (gameState.result === "win") {
+    message.className = "win-message";
+    message.textContent = `You won ${playerControl.activePlayer().name}!`;
   } else {
-    message.classList.add("win");
-    message.textContent = `You won ${activePlayer().name}!`;
+    message.className = "draw-message";
+    message.textContent = "Draw.";
   }
 }
 
 function gameAction(choice) {
-  if (!gameState.status) return;
   getChoice(choice);
-  hasWinner();
+  resultCheck();
   updateBoard();
 }
 
-gameBoard.addEventListener("click", (event) => {
+boardDisplay.addEventListener("click", (event) => {
+  if (gameState.result) return;
   gameAction(event.target.dataset.coordination);
   event.target.disabled = true;
   event.target.style.borderColor = "gray";
@@ -137,10 +166,13 @@ gameBoard.addEventListener("click", (event) => {
 });
 
 const restartBtn = document.getElementById("restart-btn");
-const closeDialogBtn = document.getElementById("close-dialog");
+const closeDialogBtn = document.getElementById("close-dialog-btn");
 const createPlayerDialog = document.getElementById("create-player-dialog");
 
 restartBtn.addEventListener("click", () => createPlayerDialog.showModal());
 closeDialogBtn.addEventListener("click", () => createPlayerDialog.close());
 
-updateBoard();
+createPlayerDialog.showModal();
+playerControl.setPlayers();
+
+// todo: Make restart button functional
